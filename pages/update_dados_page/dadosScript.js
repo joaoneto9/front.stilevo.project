7097b1f551f8d1e9d92f67c36ebab485cb978c82
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2025-05-20 15:51:28 -0300

    feat: finalizando a pagina de atualizacao de dados de um Uusario

--- pages/update_dados_page/dadosScript.js
+++ pages/update_dados_page/dadosScript.js
@@ -1,12 +1,27 @@
 import { updateParcialDataUser } from "../../routes/user/updateDados.js";
 import { viaCepRequest } from "../function_page/viaCepRequest.js";
-import { setUser } from "../user_config/user.js";
+import { getUserEmail, getUserEndereco, getUserName, setUser } from "../user_config/user.js";
 
 const inputCep = document.getElementById("cep");
+const atributosEndereco = document.getElementById("atributosEndereco");
 
 let enderecoToUpdate = null;
 
+inputCep.addEventListener('input', () => {
+    let valor = inputCep.value.replace(/\D/g, ''); // remove tudo que não for dígito
+
+    if (valor.length > 5) {
+        valor = valor.slice(0, 5) + '-' + valor.slice(5, 8);
+    }
+
+    inputCep.value = valor;
+});
+
 inputCep.addEventListener("blur", async () => {
+    atributosEndereco.innerHTML = '';
+    atributosEndereco.style.color = 'black';
+    // limpa ess campo
+
     const valor = inputCep.value.trim();
 
     if (valor == "")
@@ -14,6 +29,10 @@ inputCep.addEventListener("blur", async () => {
 
     try {
         const enderecoViaCep = await viaCepRequest(valor);
+
+        if (enderecoViaCep.erro) {
+            throw new error("cep invalido"); 
+        }
 
         const endereco = {
             cep: enderecoViaCep.cep,
@@ -26,60 +45,19 @@ inputCep.addEventListener("blur", async () => {
             pontoReferencia: null
         }
 
-        const atributosEndereco = document.getElementById("atributosEndereco");
+        showEndereco(endereco);
 
-        Object.entries(endereco).forEach(([atributo, valor]) => {
-            if (atributo === "cep")
-                return; // pula pro próximo
-
-            // tenta achar o input existente pelo id
-            let inputExistente = document.getElementById(atributo);
-
-            if (inputExistente) {
-                // só atualiza o valor se já existe
-                inputExistente.value = valor !== null ? valor : "";
-                return; // troca o valor e passa pro proximo ja 
-            }
-
-            // cria o grupo do zero se não existe
-            const div = document.createElement("div");
-            div.className = "form-group";
-
-            const label = document.createElement("label");
-            label.setAttribute("for", atributo);
-            label.innerText = atributo.charAt(0).toUpperCase() + atributo.slice(1) + ":";
-
-            const input = document.createElement("input");
-
-            input.type = "text";
-            input.id = atributo;
-            input.name = atributo;
-            input.value = valor !== null ? valor : "";
-
-            if (atributo === "numero") {
-                input.placeholder = '540';
-                input.type = "number"; // provavelmente era pra ser número
-            }
-
-            if (atributo === "uf") {
-                input.maxLength = 2;
-                input.readOnly = true;
-            }
-
-            if (atributo === "localidade" || atributo === "bairro") {
-                input.readOnly = true;
-            }
-
-            div.appendChild(label);
-            div.appendChild(input);
-            atributosEndereco.appendChild(div);
-
-            enderecoToUpdate = endereco;
-            return;
-        });
+        enderecoToUpdate = endereco;
+        return;
+        
     } catch (error) {
         console.log("ERRO: " + error);
-        atributosEndereco.innerHTML = '';
+        atributosEndereco.innerHTML = `
+            <small>CEP invalido, tente novamente.</small>
+            <br></br>
+        `;
+        atributosEndereco.style.color = 'red';
+
         enderecoToUpdate = null;
         return;
     }
@@ -131,4 +109,74 @@ form.addEventListener('submit', async (event) => {
         showConfirmButton: false
     });
 
+});
+
+function showEndereco(endereco) {
+
+    Object.entries(endereco).forEach(([atributo, valor]) => {
+        if (atributo === "cep")
+            return; // pula pro próximo
+
+        // tenta achar o input existente pelo id
+        let inputExistente = document.getElementById(atributo);
+
+        if (inputExistente) {
+            // só atualiza o valor se já existe
+            inputExistente.value = valor !== null ? valor : "";
+            return; // troca o valor e passa pro proximo ja 
+        }
+
+        // cria o grupo do zero se não existe
+        const div = document.createElement("div");
+        div.className = "form-group";
+
+        const label = document.createElement("label");
+        label.setAttribute("for", atributo);
+        label.innerText = atributo.charAt(0).toUpperCase() + atributo.slice(1) + ":";
+
+        const input = document.createElement("input");
+
+        input.type = "text";
+        input.id = atributo;
+        input.name = atributo;
+        input.value = valor !== null ? valor : "";
+
+        if (atributo === "numero") {
+            input.placeholder = 'numero da residencia (ex: 540)';
+            input.type = "number"; // provavelmente era pra ser número
+        }
+
+        if (atributo === "uf") {
+            input.maxLength = 2;
+            input.readOnly = true;
+        }
+
+        if (atributo === "localidade" || atributo === "bairro") {
+            input.readOnly = true;
+        }
+
+        div.appendChild(label);
+        div.appendChild(input);
+        atributosEndereco.appendChild(div);
+    })
+}
+
+    
+window.addEventListener('DOMContentLoaded', () => {
+        const nameInput = document.getElementById("name");
+        const emailInput = document.getElementById("email");
+
+        if (getUserEmail() == undefined && getUserEmail() == undefined) {
+            return;
+        }
+
+        emailInput.value = getUserEmail();
+        nameInput.value = getUserName()
+
+        if (getUserEndereco() == undefined) {
+            return;
+        }
+
+        inputCep.value = getUserEndereco().cep;
+        showEndereco(getUserEndereco());
 });
